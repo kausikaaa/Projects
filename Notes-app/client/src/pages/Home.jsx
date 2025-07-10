9a5e0f4c96fa5c34df117ea2b0a06eaaef79cfc0
kausikaaa <kauss0806@gmail.com>
2025-07-10 23:46:02 +0530

    feat(frontend) : update home page

---
+++ Notes-app/client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FilterBar from '../components/FilterBar';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <FilterBar />
+    </div>
+  );
+};
+
+export default Home;
