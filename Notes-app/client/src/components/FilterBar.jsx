86375c70837b0c0419ec93a0010acfade7fa06b6
kausikaaa <kauss0806@gmail.com>
2025-07-10 23:27:53 +0530

    feat(frontend) : add filterBar component

---
+++ Notes-app/client/src/components/FilterBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const FilterBar = () => {
+  return (
+    <div className="filter-bar">
+      <input type="text" placeholder="Search by title or content..." />
+      <select>
+        <option value="">All Tags</option>
+        <option value="Work">Work</option>
+        <option value="Personal">Personal</option>
+      </select>
+    </div>
+  );
+};
+
+export default FilterBar;
