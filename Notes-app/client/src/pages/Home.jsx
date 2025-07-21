9a0fe0205a4491c6be5649882707acef3246395b
kausikaaa <kauss0806@gmail.com>
2025-07-21 22:56:44 +0530

    feat(frontend) : add style for component a nd update home page

--- Notes-app/client/src/pages/Home.jsx
+++ Notes-app/client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import FilterBar from '../components/FilterBar';
+import AddNoteForm from '../components/AddNoteForm';
 
 const Home = () => {
+
   return (
-    <div className ="home-container">
+    <div className="home-container">
+
+      <AddNoteForm onNoteAdded={(newNote) => {
+  // to push to notes list or refetch notes later
+}} />
       <FilterBar />
     </div>
   );
